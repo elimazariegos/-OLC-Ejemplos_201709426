d65d7dcaaaa798168da20da6f639c0883f8d7497
Eli Mazariegos <elimazariegosr1@gmail.com>
2019-08-14 01:48:59 -0600

    Analizador lexico para palabras reservadas, identificadores y numeros usando jflex

---
+++ Analizador/src/codigo/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+import java.io.File;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Principal {
+    public static void main(String[] args) {
+        String ruta = "C:/Users/Samuel/Documents/NetBeansProjects/Analizador/src/codigo/Lexer.flex";
+        generarLexer(ruta);
+    }
+    public static void generarLexer(String ruta){
+        File archivo = new File(ruta);
+        JFlex.Main.generate(archivo);
+        
+    }
+        
+}
